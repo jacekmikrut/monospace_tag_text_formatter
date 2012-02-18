d7d44aa422cec0d79ec27344840af5d91cfef59e
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-02-18 12:16:17 +0100

    Made MonospaceTagTextFormatter::Box use MonospaceTagTextFormatter::Line class objects.

--- lib/monospace_tag_text_formatter/box.rb
+++ lib/monospace_tag_text_formatter/box.rb
@@ -6,5 +6,9 @@ module MonospaceTagTextFormatter
     def string_to_chunk(string)
       Chunk.new(string)
     end
+
+    def new_line(string_or_chunk, attrs={})
+      Line.new(string_or_chunk, attrs)
+    end
   end
 end
--- spec/monospace_tag_text_formatter/box_spec.rb
+++ spec/monospace_tag_text_formatter/box_spec.rb
@@ -11,4 +11,40 @@ describe MonospaceTagTextFormatter::Box do
   describe "@omission" do
     it { subject.instance_variable_get("@omission").should be_a(MonospaceTagTextFormatter::Chunk) }
   end
+
+  describe "@empty_top_lines" do
+    context "when :valign is :middle" do
+      before { subject.attributes(:height => 3, :valign => :middle) }
+      it { subject.send(:empty_top_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+
+    context "when :valign is :bottom" do
+      before { subject.attributes(:height => 2, :valign => :bottom) }
+      it { subject.send(:empty_top_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+  end
+
+  describe "@content_lines" do
+    context 'when the text is ""' do
+      subject { described_class.new("") }
+      it { subject.send(:content_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+
+    context 'when the text is "This is some text."' do
+      subject { described_class.new("This is some text.") }
+      it { subject.send(:content_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+  end
+
+  describe "@empty_bottom_lines" do
+    context "when :valign is :top" do
+      before { subject.attributes(:height => 2, :valign => :top) }
+      it { subject.send(:empty_bottom_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+
+    context "when :valign is :middle" do
+      before { subject.attributes(:height => 3, :valign => :middle) }
+      it { subject.send(:empty_bottom_lines).first.should be_a(MonospaceTagTextFormatter::Line) }
+    end
+  end
 end
