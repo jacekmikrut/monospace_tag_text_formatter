d46ade84f6ea0bc1c00be10a9f5a182223c17f13
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-02-18 14:27:30 +0100

    Added more specs for MonospaceTagTextFormatter::Line class.

--- spec/monospace_tag_text_formatter/line_spec.rb
+++ spec/monospace_tag_text_formatter/line_spec.rb
@@ -11,4 +11,32 @@ describe MonospaceTagTextFormatter::Line do
   describe "@omission" do
     it { subject.instance_variable_get("@omission").should be_a(MonospaceTagTextFormatter::Chunk) }
   end
+
+  describe "#width=" do
+
+    subject { MonospaceTagTextFormatter::Line.new("<tag>This is some text.</tag>") }
+
+    it "should update the width" do
+
+      subject.width = 20
+      subject.width.should == 20
+      subject.to_s.should == "<tag>This is some text.</tag>  "
+
+      subject.width = 18
+      subject.width.should == 18
+      subject.to_s.should == "<tag>This is some text.</tag>"
+
+      subject.width = 11
+      subject.width.should == 11
+      subject.to_s.should == "<tag>This is</tag> ..."
+
+      subject.width = 0
+      subject.width.should == 0
+      subject.to_s.should == ""
+
+      subject.width = nil
+      subject.width.should == 18
+      subject.to_s.should == "<tag>This is some text.</tag>"
+    end
+  end
 end
